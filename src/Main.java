79be1420aab8cf97cdf578bc35eacd4632ba466c
AKO-AllKonwingOne <hugo.lindgren.svensson@gmail.com>
2024-01-11 11:22:48 +0100

    Gjorde nya klasser

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
 public class Main {
 
+    // Attributes
+
+    private String color;
+    private int value;
+    private String name;
+
+    // Constructors
+
+    // Methods
+
+    // Getters and setters
+
 }
